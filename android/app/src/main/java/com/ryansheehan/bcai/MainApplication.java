ef17cb20edb9317061013b7a03a26a4395068d7d
Ryan-Sheehan <sheehan.w.ryan@gmail.com>
2021-08-10 18:36:14 +0200

    clean up

--- android/app/src/main/java/com/ryansheehan/bcai/MainApplication.java
+++ android/app/src/main/java/com/ryansheehan/bcai/MainApplication.java
@@ -1,4 +1,4 @@
-package com.bcai;
+package com.ryansheehan.bcai;
 
 import android.app.Application;
 import android.content.Context;
@@ -11,7 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.bcai.generated.BasePackageList;
+import com.ryansheehan.bcai.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -107,7 +107,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.bcai.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.ryansheehan.bcai.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
